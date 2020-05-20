a74d0a4b56371588b0e319470c5e9e9a04205d4f
zoosissoos <zoosissoos@github.com>
2020-05-19 22:14:45 -0400

    fix modal

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -5,11 +5,24 @@ import IconGithub from "../../images/icons/iconGithub";
 import IconLinkedin from "../../images/icons/iconLinkedin";
 import IconGoogleDrive from "../../images/icons/iconGoogleDrive";
 import IconGoogleGmail from "../../images/icons/iconGoogleGmail";
+import {graphql, useStaticQuery} from "gatsby";
 
 
 const Contact = () => {
+  const {site} = useStaticQuery(graphql`
+      query GithubQuery {
+          site {
+              siteMetadata {
+                  gitHubLink
+                  resumeLink
+                  emailAddress
+                  linkedInLink
+              }
+          }
+      }
+  `);
 
-  const [ open, setIsOpen ] = useState(false);
+  const [open, setIsOpen] = useState(false);
 
   return (
     <>
@@ -18,21 +31,41 @@ const Contact = () => {
         open && (
           <div className="fixed bottom-0 right-0 w-full h-screen flex">
             <div className="flex flex-col sm:flex-row m-auto w-2/4 bg-gray-200 p-2 rounded-md">
-              <div className="flex w-1/4 justify-center items-center p-2">
-                <IconGithub/>
+              <div className="flex sm:flex-col flex-row sm:w-1/4 w-full justify-center items-center p-2">
+                <a target="_blank" rel="noopener noreferrer" href={site.siteMetadata.gitHubLink}
+                   className="w-2/4 h-auto">
+                  <IconGithub/>
+                </a>
+                <div className="w-2/4 sm: w-full text-center">
+                  <p>GitHub</p>
+                </div>
               </div>
-              <div className="flex w-1/4 justify-center items-center p-2">
-
-                <IconLinkedin/>
+              <div className="flex sm:flex-col flex-row sm:w-1/4 w-full justify-center items-center p-2">
+                <a target="_blank" rel="noopener noreferrer" href={site.siteMetadata.linkedInLink}
+                   className="w-2/4 h-auto">
+                  <IconLinkedin/>
+                </a>
+                <div className="w-2/4 sm: w-full text-center">
+                  <p>LinkedIn</p>
+                </div>
               </div>
-              <div className="flex w-1/4 justify-center items-center p-2">
-                <IconGoogleDrive/>
-
+              <div className="flex sm:flex-col flex-row sm:w-1/4 w-full justify-center items-center p-2">
+                <a target="_blank" rel="noopener noreferrer" href={site.siteMetadata.resumeLink}
+                   className="w-2/4 h-auto">
+                  <IconGoogleDrive/>
+                </a>
+                <div className="w-2/4 sm: w-full text-center">
+                  <p>Resume</p>
+                </div>
               </div>
-              <div className="flex w-1/4 justify-center items-center p-2">
-
-                <IconGoogleGmail/>
-
+              <div className="flex sm:flex-col flex-row sm:w-1/4 w-full justify-center items-center p-2">
+                <a target="_blank" rel="noopener noreferrer" href={`mailto:${site.siteMetadata.emailAddress}`}
+                   className="w-2/4 h-auto">
+                  <IconGoogleGmail/>
+                </a>
+                <div className="w-2/4 sm: w-full text-center">
+                  <p>Email</p>
+                </div>
               </div>
             </div>
           </div>
